80e662c2f9509a2a9ee8ae7ed07e4ecdf6b40d42
gluizbr <bielraawr@gmail.com>
2019-08-28 08:45:27 -0300

    Add first commit on compiler

---
+++ v1/parser.h
@@ -0,0 +1,10 @@
+/*@<parser.h>::*/
+extern int lookahead;
+int gettoken(FILE*);
+void expr(void);
+void rest(void);
+void quoc(void);
+void term(void);
+void fact(void);
+void match(int);
+
---
+++ v2/tokens.h
@@ -0,0 +1,8 @@
+/*
+ * @<tokens.h>::
+ */
+enum {
+    ID = 1024,
+    DEC,
+    ASGN,
+};
