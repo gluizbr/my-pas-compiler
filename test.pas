e584ff867b96aa9fc5a28510b197f900166c2ab6
gluizbr <bielraawr@gmail.com>
2019-11-08 11:15:07 -0300

    Change error when file extension is incorrect and add more test

--- test.pas
+++ test.pas
@@ -21,10 +21,14 @@ program test (input, output);
         if(n < 2) then
           fact := 1
         else
-          fact := n * (n-1)
+          fact := n * (n-1);
+          while (n < 2) do
+            x := 1
       end
+        
   begin
-      write
+      write;
+      repeat x:= 1 until (n <= 2)
   end
 
 begin
